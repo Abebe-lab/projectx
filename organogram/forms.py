7848a0ab9427ac1d0d37c859457cc669c460a725
Abebe-lab <abebezwdu@gmail.com>
2025-02-17 16:52:49 +0300

    added changes to forms.py under organogram app

--- organogram/forms.py
+++ organogram/forms.py
@@ -9,7 +9,8 @@ class CustomerForm(forms.ModelForm):
     name_am = forms.CharField(required=True, label=mark_safe("የአማርኛ ስም <span style='color: red'>*</span>"),
                               widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "የአማርኛ ስም"}))
     description = forms.CharField(required=False, label="Description",
-                                  widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "Description", "rows": 4}))
+                                  widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "Description", "rows": 4,  "style": "width: 100%;"}))
+
     contact = forms.CharField(required=False, label="Contact",
                               widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Contact"}))
     customer_type = forms.ChoiceField(choices=[('', 'Select Customer Type'), ('Org', 'Organization'),
