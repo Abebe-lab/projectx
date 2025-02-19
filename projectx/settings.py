9ef92acf2bf500630b89935fddd8237297220443
Abebe-lab <abebezwdu@gmail.com>
2025-02-19 19:22:32 +0300

    settings.py in projectx app

--- projectx/settings.py
+++ projectx/settings.py
@@ -114,15 +114,26 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASS'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': '5432',
+# }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': 'projectx',
+        'USER': 'projectx_user',
+        'PASSWORD': 'projectx',
+        'HOST': 'localhost',
         'PORT': '5432',
-}
+    }
 }
 
 
