ee25185dee33085e27ee74a4a34df3234de6e144
Abebe-lab <abebezwdu@gmail.com>
2025-02-20 21:48:06 +0300

    settings.py in projectx app

--- projectx/settings.py
+++ projectx/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
 from pathlib import Path
 from firebase_admin import initialize_app, credentials
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
